62baaf40e9adff94e72ffe20ff557c599be0365c
Rofiu Mujeeb Olanrewaju <mujeeb.rofiu@gmail.com>
2023-08-11 10:44:29 -0700

    task 5 0x00-hello_world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*The first line is the main entry sentence
+*
+*Return: Its always 0 (depicts success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+}
