03cb7c21aa7eb80b39928b25354e7298235d28b3
Rofiu Mujeeb Olanrewaju <mujeeb.rofiu@gmail.com>
2023-08-11 00:38:30 -0700

    task 4 0x00-hello_world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*The first line is the main entry sentence
+*
+*Return: Its always 0 (depicts success)
+*/
+int main(void)
+{
+        Puts("Programming is like building a multilingual puzzle\n");
+        return (0);
+}
