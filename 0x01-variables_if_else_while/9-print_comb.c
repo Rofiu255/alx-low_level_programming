7a995a12cb3b8d7ad422e05adadb4053b58e34aa
Rofiu Mujeeb Olanrewaju <mujeeb.rofiu@gmail.com>
2023-08-13 05:38:56 -0700

    task 9 0x01-variable_if.....

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - printing all single digits from 0 to 10
+ *
+ * Return: Always 0 (depict Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar("%d", num);
+		if (num == 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
