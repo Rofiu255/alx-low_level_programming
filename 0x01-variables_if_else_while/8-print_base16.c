308181a641ac210a446a2c22f5a3bef9bd7f5ca4
Rofiu Mujeeb Olanrewaju <mujeeb.rofiu@gmail.com>
2023-08-13 04:28:39 -0700

    task 7 0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+/**
+ * main - printing all digits in hexadecimal from 0 to 15 with lowercase letter
+ *
+ * Return: Always 0 (depict Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 16; num++)
+	{
+		putchar(("%x", num);
+	}
+	putchar('\n');
+	return (0);
+}
