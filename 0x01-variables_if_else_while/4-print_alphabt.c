2d5a771c104879f5ad5caa16f5bcd87cdd450138
Rofiu Mujeeb Olanrewaju <mujeeb.rofiu@gmail.com>
2023-08-13 03:01:35 -0700

    task 4 0x01-variable_if_else_while

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,16 +1,15 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
- * main - Printing of alphabets using putchar only.
+ * main - Printing of alphabets except q and e using putchar only.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char alpha;
-	
+
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
 		if (alpha != 'q' && alpha != 'e')
