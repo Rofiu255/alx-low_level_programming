32388a26aa929763ddee85f83775d5343d336e10
Rofiu Mujeeb Olanrewaju <mujeeb.rofiu@gmail.com>
2023-08-12 20:38:34 -0700

    task 2 0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Printing of alphabets using putchar only.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha[26] = "abcdefghijklmnopqrstuvxwy";
+	int n;
+
+	for (i = 0, i < 26, i++)
+	{
+		putchar(alpha[i]);
+	}
+	putchar("\n");
+	return (0);
+}
