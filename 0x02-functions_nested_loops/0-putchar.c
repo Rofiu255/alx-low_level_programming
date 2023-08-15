414b1b41242ac700b7a9eacd6fedd9a0e85755a9
Rofiu Mujeeb Olanrewaju <mujeeb.rofiu@gmail.com>
2023-08-15 14:53:22 -0700

    task 0 0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that print _putchar followed by new line.
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	putchar("_putchar");
+	putchar('\n');
+	return (0);
+}
