56003d0daa1af43b325129f6bccfe1afae20977e
Rofiu Mujeeb Olanrewaju <mujeeb.rofiu@gmail.com>
2023-08-16 02:27:08 -0700

    update

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_alphabet_x10 - to print alphabet in lowercase 10 times
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+	}
+	_putchar('\n');
+}
