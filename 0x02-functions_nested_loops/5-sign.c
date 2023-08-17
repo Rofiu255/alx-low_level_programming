1b5ab6a2bd24f9b2c2cc4d0e0c3e8bace4f8ba60
Rofiu Mujeeb Olanrewaju <mujeeb.rofiu@gmail.com>
2023-08-17 02:28:08 -0700

    update

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_sign - Printing the sign of a number either positive or negative.
+ *
+ * @n: it is an integer with either positive or negative or zero.
+ *
+ * Return: 1 for positive num, -1 for negative num, 0 for zero.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return ('1');
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (-1);
+	}
+}
