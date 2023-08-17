f115ec174cfe51958626a43d8434092ec37be15a
Rofiu Mujeeb Olanrewaju <mujeeb.rofiu@gmail.com>
2023-08-17 02:34:30 -0700

    update

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,23 +5,23 @@
  *
  * @n: it is an integer with either positive or negative or zero.
  *
- * Return: 1 for positive num, -1 for negative num, 0 for zero.
+ * Return: +1 for positive num, -1 for negative num, 0 for zero.
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar(43);
+		putchar(43);
 		return ('1');
 	}
 	else if (n < 0)
 	{
-		_putchar(45);
+		putchar(45);
 		return (-1);
 	}
 	else
 	{
-		_putchar(48);
+		putchar(48);
 		return (-1);
 	}
 }
