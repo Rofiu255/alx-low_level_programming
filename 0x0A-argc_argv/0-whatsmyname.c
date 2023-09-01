01068fe9d43698529c381dcebf8280eb367e5e00
Rofiu Mujeeb Olanrewaju <mujeeb.rofiu@gmail.com>
2023-09-01 10:32:18 -0700

    update

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,8 +8,10 @@
  *
  * Return: return the name of the file name
  */
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
+	(void) argc;
+
 	printf("%s \n", argv[0]);
 	return (0);
 }
