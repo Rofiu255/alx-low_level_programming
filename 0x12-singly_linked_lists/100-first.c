460c4b2194a2abd82c7221241ccf8807b99dff00
Rofiu255 <mujeeb.rofiu@gmail.com>
2023-09-21 20:51:00 -0700

    updatte

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void __attribute__((constructor)) _print_first(void);
+
+/**
+ * _print_firt - Prints a string before the
+ *        main function is executed.
+ */
+void _print_first(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
+}
