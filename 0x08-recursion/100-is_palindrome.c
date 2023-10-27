94ba0b5f01424e3066dd8d14dd5ff7e92ee8accc
Rofiu255 <mujeeb.rofiu@gmail.com>
2023-10-26 22:46:43 -0700

    updated

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,47 +2,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int find_strlen(char *s);
+int findIf_palindrome(char *s, int str_len, int i);
 int is_palindrome(char *s);
 
 /**
- * is_palindrome - Checks if a string is a palindrome.
- * @s: The string to be checked.
-
- *
- * Return: 1 if s is a palindrome, 0 otherwise.
- */
-int is_palindrome(char *s)
-{
-	int length = _strlen(s);
-	int i;
-	if (!s)
-		return (1);
-	for (i = 0; i < length / 2; i++)
-	{
-		if (s[i] != s[length - i - 1])
-		{
-			return (0);
-			/**
-			 * Not a palindrome
-			 */
-		}
-	}
-	return (1);/**It's a palindrome */
-}
-
-/**
- * _strlen - Calculate the length of a string.
+ * find_strlen - Returns the length of a string.
  * @s: The string to be measured.
  *
  * Return: The length of the string.
  */
-int _strlen(char *s)
+int find_strlen(char *s)
 {
-	int length = 0;
+	int str_len = 0;
 
-	while (s[length] != '\0')
+	if (*(s + str_len))
 	{
-		length++;
+		str_len++;
+		str_len += find_strlen(s + str_len);
 	}
-	return (length);
+
+	return (str_len);
+}
+
+/**
+ * findIf_palindrome - Checks if a string is a palindrome.
+ * @s: The string to be checked.
+ * @str_len: The length of s.
+ * @i: The index of the string to be checked.
+ *
+ * Return: If the string is a palindrome - 1.
+ *         If the string is not a palindrome - 0.
+ */
+int findIf_palindrome(char *s, int str_len, int i)
+{
+	if (s[i] == s[str_len / 2])
+		return (1);
+
+	if (s[i] == s[str_len - i - 1])
+		return (findIf_palindrome(s, str_len, i + 1));
+
+	return (0);
+}
+
+/**
+ * is_palindrome - Checks if a string is a palindrome.
+ * @s: The string to be checked.
+ *
+ * Return: If the string is a palindrome - 1.
+ i*         If the string is not a palindrome - 0.
+ */
+int is_palindrome(char *s)
+{
+	int i = 0;
+	int str_len = find_strlen(s);
+
+	if (!(*s))
+		return (1);
+
+	return (findIf_palindrome(s, str_len, i));
 }
