8c993483b23e882441ecb6e467d7ab17c99783dd
Rofiu255 <mujeeb.rofiu@gmail.com>
2023-11-03 08:04:45 -0700

    updatd

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,5 +7,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int get_strlen(char *str);
+char *create_xarr(int size);
+char *iter_zeroes(char *str);
+void find_mul(char *prod, char *mult, int digit, int zeroes);
+void sum_nums(char *final_prod, char *next_prod, int next_len);
 
 #endif
